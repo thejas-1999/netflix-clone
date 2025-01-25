4a5f00b4ba7b5b62490bafb0fcdf23440b3a0601
Thejas K <143934668+thejas-1999@users.noreply.github.com>
2025-01-25 11:10:53 +0530

    create navbar

--- src/main.jsx
+++ src/main.jsx
@@ -9,11 +9,15 @@ import {
 import "./index.css";
 import App from "./App.jsx";
 import LoginScreen from "./pages/Login/LoginScreen.jsx";
+import HomeScreen from "./pages/Home/HomeScreen.jsx";
+import PlayerScreen from "./pages/Player/PlayerScreen.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
-      <Route index={true} path="/" element={<LoginScreen />} />
+      <Route index={true} path="/" element={<HomeScreen />} />
+      <Route path="/login" element={<LoginScreen />} />
+      <Route path="/player" element={<PlayerScreen />} />
     </Route>
   )
 );
